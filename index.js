4c7ea32760f65b9658820550d72bc68d7aa2de5e
rafidaky <rafidakn@gmail.com>
2024-01-27 15:36:40 +0100

    cors enabled

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const axios = require("axios");
+var cors = require("cors");
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
